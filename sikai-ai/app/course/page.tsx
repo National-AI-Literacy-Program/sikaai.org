2d20bdf6e30eef0bb58790baa9d33775464b1f4d
mandeep-bhandari123 <mandeepbhandari892@gmail.com>
2026-08-25 17:39:59 +0545

    Added the data of day 1

---
+++ sikai-ai/app/course/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function CoursePage() {
+  return (
+    <main>
+      <h1>4 Weeks AI Literacy Course</h1>
+
+      <p>Start your AI learning journey.</p>
+
+      <Link href="course/1">
+            Start Day 1
+      </Link>
+    </main>
+  );
+}
